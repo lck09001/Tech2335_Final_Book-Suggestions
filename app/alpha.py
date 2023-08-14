abec57d9f9b18c88f4757f4d9599b6194860ca8f
lck09001 <lck249@nyu.edu>
2023-08-14 13:44:18 -0400

    update the test file

--- app/alpha.py
+++ app/alpha.py
@@ -4,5 +4,3 @@ from dotenv import load_dotenv
 load_dotenv() #> invoking this function loads contents of the ".env" file into the script's environment...
 
 API_KEY = os.getenv("BOOKS_API_KEY")
-
-
--- test/book_suggestions_test.py
+++ test/book_suggestions_test.py
@@ -3,4 +3,32 @@
 from app.book_suggestions import search_books, display_book_details, suggest_random_book
 
 
+# Function to search for books
+def search_books(query):
+    params = {'q': query, 'key': API_KEY}
+    response = requests.get(BASE_URL, params=params)
+    data = response.json()
+    return data
+
+
+# Function to display book details
+def display_book_details(book):
+    volume_info = book.get('volumeInfo', {})
+    title = volume_info.get('title', 'No Title')
+    authors = volume_info.get('authors', ['Unknown Author'])
+    description = volume_info.get('description', 'No description available')
+    print(f"Title: {title}")
+    print(f"Authors: {', '.join(authors)}")
+    print(f"Description: {description}")
+    print("=" * 20)
+
+
+# Function to suggest a random book
+def suggest_random_book(books_data):
+    if 'items' in books_data:
+        random_book = random.choice(books_data['items'])
+        display_book_details(random_book)
+    else:
+        print("Sorry, no books found for your query.")
+
 
